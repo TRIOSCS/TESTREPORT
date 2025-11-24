29cd45ec338f66f74824b99f43bf405dc8715ec2
KishanChauhan <Kishanchauhan00007@gmail.com>
2025-11-24 17:59:51 +0530

    updated allowed hosts

--- drivehealth/settings.py
+++ drivehealth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5#-xy^a7x6r6*a=gqsh)cempr*gyd0opy)8d4!6bjzu#k-v31w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
